a484eeb01e37009b423d70f8b858ff20f2eb554b
Aljith KJ <kwdeathlok@gmail.com>
2023-04-07 21:28:54 +0530

    Notes added

---
+++ 4 Talk_is_cheap_show_me the_ code/my-react-app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const heading = React.createElement('h1', {
+    id: 'title'
+}, "Hello World!");
+root.render(heading);
+
